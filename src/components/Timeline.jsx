6d7fe16659dafce4047ae2ec273dc8fef9bda91d
Brandon Tsai <tsaibr@oregonstate.edu>
2025-12-25 18:16:56 -0800

    implements timeline item and basic card format

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,10 +3,12 @@ import YearSection from "./YearSection";
 
 const Timeline = () => {
   return (
-    <div className="max-w-5xl mx-auto py-12 px-4 space-y-16">
-      {memories.map((year, index) => (
-        <YearSection key={year.year} index={index} {...year}/>
-      ))}
+    <div className="flex justify-center">
+      <div className="max-w-5xl mx-auto py-12 px-4 space-y-16">
+        {memories.map((year, index) => (
+          <YearSection key={year.year} index={index} {...year}/>
+        ))}
+      </div>
     </div>
   )
 }
