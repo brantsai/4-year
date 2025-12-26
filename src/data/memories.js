a962cd2c63e20e9e07484ca0b2c5e6f148179211
Brandon Tsai <tsaibr@oregonstate.edu>
2025-12-25 21:31:47 -0800

    completed year 0

--- src/data/memories.js
+++ src/data/memories.js
@@ -1,6 +1,10 @@
 import first_date from "../assets/year-0/first-date.jpg"
 import second_date from "../assets/year-0/second-date.JPG"
 import zedd from "../assets/year-0/zedd.JPG"
+import flea_market from "../assets/year-0/flea-market.JPG"
+import sf_hill from "../assets/year-0/sf-hill.JPG"
+import davis_lawn from "../assets/year-0/davis-lawn.JPG"
+import anniversary_0 from "../assets/year-0/0-anniversary.JPG"
 
 const memories = [
   {
@@ -19,34 +23,34 @@ const memories = [
         image: second_date,
       },
       {
-        title: "second date",
-        date: "July 4, 2021",
-        description: "our second date!! back when we could just chug whole bottles of soju at once... you'll notice that we are still unfortunately not pictured yet. i think we were just awkward so we didn't take any pictures together XD. i remember you were wearing a blue top and you looked very pretty :)! you might've worn the same top on our first date too LOL not sure actually",
-        image: second_date,
+        title: "first rave together",
+        date: "August 8, 2021",
+        description: "our first rave!! and our first picture together (we also had a polaroid that we took before but i like this better). i had so much with you at zedd hehe and this was also technically the first time you met all my friends! LOL",
+        image: zedd,
       },
       {
-        title: "second date",
-        date: "July 4, 2021",
-        description: "our second date!! back when we could just chug whole bottles of soju at once... you'll notice that we are still unfortunately not pictured yet. i think we were just awkward so we didn't take any pictures together XD. i remember you were wearing a blue top and you looked very pretty :)! you might've worn the same top on our first date too LOL not sure actually",
-        image: second_date,
+        title: "flea market w/ katherine + eric",
+        date: "August 13, 2021",
+        description: "honestly i didn't have to include this one but i just thought it was funny. and we look cool in this pic too :p. super uncomfortable meal at jollibees but it was a fun hangout and was nice to meet your friends for the first time!",
+        image: flea_market,
       },
       {
-        title: "second date",
-        date: "July 4, 2021",
-        description: "our second date!! back when we could just chug whole bottles of soju at once... you'll notice that we are still unfortunately not pictured yet. i think we were just awkward so we didn't take any pictures together XD. i remember you were wearing a blue top and you looked very pretty :)! you might've worn the same top on our first date too LOL not sure actually",
-        image: second_date,
+        title: "sf date :')",
+        date: "August 25, 2021",
+        description: "my fav picture from our early days :) this was such a sad day but it was also a rly fun date. i think this is the moment where i started to realize how much i liked you, but it was also very bittersweet because we both thought we weren't going to see eachother anymore after you left. also i totally forgot that you photoshopped this pic to include the view LMAO",
+        image: sf_hill,
       },
       {
-        title: "second date",
-        date: "July 4, 2021",
-        description: "our second date!! back when we could just chug whole bottles of soju at once... you'll notice that we are still unfortunately not pictured yet. i think we were just awkward so we didn't take any pictures together XD. i remember you were wearing a blue top and you looked very pretty :)! you might've worn the same top on our first date too LOL not sure actually",
-        image: second_date,
+        title: "davis",
+        date: "September 4, 2021",
+        description: "my first time visiting you in davis!! this weekend was so perfect, and i always think back to it so fondly. i had so much fun exploring davis with you and moving you in, laying on the lawn together, and going to the rooftop bar in winters. i think this was a defining moment for our relationship and it's probably the point where i started to fall for you :') i feel like we were both sort of expecting the worst after leaving but i never really thought things would end for us in reality",
+        image: davis_lawn,
       },
       {
-        title: "second date",
-        date: "July 4, 2021",
-        description: "our second date!! back when we could just chug whole bottles of soju at once... you'll notice that we are still unfortunately not pictured yet. i think we were just awkward so we didn't take any pictures together XD. i remember you were wearing a blue top and you looked very pretty :)! you might've worn the same top on our first date too LOL not sure actually",
-        image: second_date,
+        title: "0th anniversary",
+        date: "December 26, 2021",
+        description: "fast forward a few months and this is when our relationship was officially born!! our 0th birthday together :)). 4 years ago from this day. we went out to see the christmas lights and it was so cold but i still had lots of fun with you. im sorry i didn't ask you to be my girlfriend in a cooler way but i couldn't help it!! it just felt right at the time. and maybe i should have asked you out in the summer instead.",
+        image: anniversary_0,
       },
     ],
   },
