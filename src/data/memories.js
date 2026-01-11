baf7fcde7729a70989647225cf38a97f43dec166
Brandon Tsai <tsaibr@oregonstate.edu>
2026-01-10 21:55:35 -0800

    implements year 0 and year 1

--- src/data/memories.js
+++ src/data/memories.js
@@ -1,16 +1,31 @@
+// YEAR 0
+
 import first_date from "../assets/year-0/first-date.jpg";
 import second_date from "../assets/year-0/second-date.JPG";
 import zedd from "../assets/year-0/zedd.JPG"
 import flea_market from "../assets/year-0/flea-market.JPG";
 import sf_hill from "../assets/year-0/sf-hill.JPG"
 import davis_lawn from "../assets/year-0/davis-lawn.JPG";
-import anniversary_0 from "../assets/year-0/0-anniversary.JPG";
-import first_trip from "../assets/year-0/first-trip.JPG";
-import new_years from "../assets/year-0/new-years.JPG";
-import conservatory from "../assets/year-0/conservatory.JPG";
-import academy_of_science from "../assets/year-0/academy-of-science.JPG";
-import ferris_wheel from "../assets/year-0/ferris-wheel.JPG";
-import lantern from "../assets/year-0/lantern.JPG";
+import painting from "../assets/year-0/painting.JPG";
+import davis_mirror from "../assets/year-0/davis-mirror.JPG";
+
+// YEAR 1
+
+import anniversary_0 from "../assets/year-1/0-anniversary.JPG";
+import first_trip from "../assets/year-1/first-trip.JPG";
+import new_years from "../assets/year-1/new-years.JPG";
+import conservatory from "../assets/year-1/conservatory.JPG";
+import academy_of_science from "../assets/year-1/academy-of-science.JPG";
+import ferris_wheel from "../assets/year-1/ferris-wheel.JPG";
+import lantern from "../assets/year-1/lantern.JPG";
+import lands_end from "../assets/year-1/lands-end.JPG";
+import picnic_day from "../assets/year-1/picnic-day.JPG";
+import garden_of_eden from "../assets/year-1/garden-of-eden.JPG";
+import in_n_out from "../assets/year-1/in-n-out.JPG";
+import fort from "../assets/year-1/fort.JPG";
+import water_temple from "../assets/year-1/water-temple.JPG";
+import hospital from "../assets/year-1/hospital.JPG";
+import la_trip from "../assets/year-1/la-trip.JPG";
 
 
 const memories = [
@@ -54,9 +69,21 @@ const memories = [
         image: davis_lawn,
       },
       {
+        title: "davis flea market",
+        date: "October 16, 2021",
+        description: "turns out we kept hanging out! who would've guessed xD i loved visiting you at davis and spending time with you while exploring the town. i've grown to love davis so much over the years we spent together there, and it holds a very special place in my heart for our early relationship. this photo was from when we visited the davis flea market! what a cute selfie :D",
+        image: davis_mirror
+      },
+      {
+        title: "painting date",
+        date: "November 23, 2021",
+        description: "our painting date! i had so much fun painting with you :) and what a beautiful view too. sorry for effing up my part of the paintings but luckily you always ended up saving them.",
+        image: painting
+      },
+      {
         title: "0th anniversary",
         date: "December 26, 2021",
-        description: "fast forward a few months and this is when our relationship was officially born!! our 0th birthday together :)). 4 years ago from this day. we went out to see the christmas lights and it was so cold but i still had lots of fun with you. im sorry i didn't ask you to be my girlfriend in a cooler way but i couldn't help it!! it just felt right at the time. and maybe i should have asked you out in the summer instead.",
+        description: "this is when our relationship was officially born!! our 0th birthday together :)). 4 years ago from this day. we went out to see the christmas lights and it was so cold but i still had lots of fun with you. im sorry i didn't ask you to be my girlfriend in a cooler way but i couldn't help it!! it just felt right at the time. and maybe i should have asked you out in the summer instead.",
         image: anniversary_0,
       },
     ],
@@ -89,16 +116,64 @@ const memories = [
         image: ferris_wheel,
       },
       {
+        title: "more sf dates - land's end",
+        date: "February 18, 2022",
+        description: "my favorite selfie of us! this picture was on one of our many dates in sf. we're watching the sunset right now sitting in the parking lot overlooking land's end. we look so happy :)! i still use this picture for some of my profile pics HAHA",
+        image: lands_end
+      },
+      {
         title: "first trip together + your bday!",
         date: "March 25, 2022",
         description: "our first trip as a couple! we got our first airbnb together in LA and celebrated your 20th birthday at beyond wonderland XD. i'm happy i got to show you around SB too and all the places i used to visit in college. also just a fun little milestone for our relationship. our first birthdays together and first trip. the start of many fun trips with each other :) happy birthday baby!",
         image: first_trip,
       },
       {
+        title: "first picnic day",
+        date: "April 23, 2022",
+        description: "our first picnic day! i'm glad we did some wholesome stuff that day before going to the daygers. we got to see baby chickers! they were so cute. definitely a highlight because i don't remember much about the parties we went to LOL. let's go back to visit davis on picnic day some time :)",
+        image: picnic_day
+      },
+      {
+        title: "garden of eden",
+        date: "May 15, 2022",
+        description: "one of my favorite places to visit with you! it's always so fun going on adventures together and exploring nature. this place is so beautiful and we honestly haven't visited here enough times. i remember trying to dip our feet into the water and it was sooo cold... it's such a long journey to get here but it's definitely worth it every time.",
+        image: garden_of_eden
+      },
+      {
+        title: "in-n-out",
+        date: "May 31, 2022",
+        description: "IN N OUT!!!! my favorite place to eat with you. thank you for always coming here with me, even though i know that most times i'm way more excited about it than you are. sorry XDD",
+        image: in_n_out
+      },
+      {
+        title: "the date you planned for me",
+        date: "August 26, 2022",
+        description: "this was the date you planned for me!! i had so much fun this day and i'm grateful that you took the time to plan everything for me :) i love the photos we took this day bc our outfits were both so flowy and bright. i also had lots of fun painting our tote bags! thank u again for helping me out with mine XD and the lego museum was such a fun experience too.",
+        image: water_temple,
+      },
+      {
+        title: "fort",
+        date: "September 21, 2022",
+        description: "this was our fort day!!!!! sooo fun and i felt just like a kid again (even tho i never built a fort as a kid). there's something about turning your room into a fort that just feels so freeing. to be honest it was a bit hard to get anything done after a while but who cares because it was so fun :) i'm glad we were able to build a fort together.",
+        image: fort,
+      },
+      {
         title: "lantern festival",
         date: "October 8, 2022",
         description: "this is us at the water lantern festival! this was such a beautiful experience and i'm so glad we were able to share it together. i loved releasing our little lanterns in the water and seeing them light up at night. let's do a sky paper lantern festival next time :)",
         image: lantern,
+      },
+      {
+        title: "hospital :(",
+        date: "November 13, 2022",
+        description: "you poor poor baby :( i wish i could've been there for you the entire step of the way, i can't believe you went through all of that yourself. you are so strong and i'm so proud of you for getting through your appendicitis AND even getting through your finals afterwards... LOL. i don't know what i would even do if anything happened to you that day :( it's so crazy to think about that so i just try not to dwell on it much and just be thankful that i still have you here with me today :)",
+        image: hospital,
+      },
+      {
+        title: "la trip",
+        date: "December 30, 2022",
+        description: "our final entry for the year and our first anniversary!! looking back, we actually did so much for this trip. i loved bundling up in our snuggies at the enchanted forest and drinking hot cocoa, i loved going to the broad with you (sort of, i think we both got bored super fast HAHA), i LOVED the aquarium with you too :) and the love museum was such a unique and memorable experience!! not to mention we got to meet up with vivi for street food which was very fun and we got to celebrate new years together! such a fun trip and a great start to our second year together.",
+        image: la_trip,
       },
     ],
   },
